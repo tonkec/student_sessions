79449a12e92968d45e943da072fcd7b2f57d3327
Antonija Simic <antonija1023@gmail.com>
2019-04-09 19:05:41 +0200

    setup routes: addSession, 404, HomePage

---
+++ src/components/AddSessionPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class AddSessionPage extends Component {
+  render() {
+    return(
+      <h1> Add New Session </h1>
+    )
+  }
+}
+
+export default AddSessionPage
