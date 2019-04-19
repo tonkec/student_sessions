c02c5d95a74b77805239d27a1d1c42aafe9925e3
Antonija Simic <antonija1023@gmail.com>
2019-04-19 22:30:18 +0200

    remove header

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
-import Header from '../components/Header';
 import Dashboard from '../components/Dashboard';
 import LoginPage from '../components/LoginPage';
 import AddSessionPage from '../components/AddSessionPage';
