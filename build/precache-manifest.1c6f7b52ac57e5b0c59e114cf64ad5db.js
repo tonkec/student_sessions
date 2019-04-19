9c00c0c771789ebf7969f77adee8390433503968
Antonija Simic <antonija1023@gmail.com>
2019-04-19 22:31:00 +0200

    build public folder; setup express server

---
+++ build/precache-manifest.1c6f7b52ac57e5b0c59e114cf64ad5db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c9f89f7ea75a696b7aa",
+    "url": "/static/css/main.606d4968.chunk.css"
+  },
+  {
+    "revision": "8c9f89f7ea75a696b7aa",
+    "url": "/static/js/main.a852e088.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c9bbaed1dd979449d1e3",
+    "url": "/static/js/2.381d5383.chunk.js"
+  },
+  {
+    "revision": "be05e7177fd170b602c229b77901823c",
+    "url": "/index.html"
+  }
+];
