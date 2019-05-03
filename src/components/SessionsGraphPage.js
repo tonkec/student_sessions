77b36a1a99bd4363c715d5a7bb07a81b40aa4b36
Antonija Simic <antonija1023@gmail.com>
2019-05-03 15:14:07 +0200

    remove filters from selected sessions

--- src/components/SessionsGraphPage.js
+++ src/components/SessionsGraphPage.js
@@ -1,33 +1,40 @@
-import React from 'react';
-import selectSessions from '../selectors/sessions';
-import selectUserTotal from '../selectors/sessions-user-total';
-import selectTypes from '../selectors/sessions-type';
-import selectProgress from '../selectors/sessions-progress';
-import BarChartSessions from './Charts/BarChartSessions';
-import PieChartSessions from './Charts/PieChartSessions';
-import {connect} from 'react-redux';
+import React from "react";
+import selectUserTotal from "../selectors/sessions-user-total";
+import selectTypes from "../selectors/sessions-type";
+import selectProgress from "../selectors/sessions-progress";
+import BarChartSessions from "./Charts/BarChartSessions";
+import PieChartSessions from "./Charts/PieChartSessions";
+import { connect } from "react-redux";
 
 class SessionsGraphPage extends React.Component {
-  render () {
+  render() {
     return (
       <div>
         <h1> Sessions Graphs </h1>
-        <BarChartSessions data={this.props.total}/>
-        <PieChartSessions data={this.props.types} dataKey="count" nameKey="type" />
-        <PieChartSessions data={this.props.progress} dataKey="count" nameKey="progress" />
+        <BarChartSessions data={this.props.total} />
+        <PieChartSessions
+          data={this.props.types}
+          dataKey="count"
+          nameKey="type"
+        />
+        <PieChartSessions
+          data={this.props.progress}
+          dataKey="count"
+          nameKey="progress"
+        />
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state,props) => {
-  const totalSessions = selectSessions(state.sessions, state.filters);
+const mapStateToProps = (state, props) => {
+  const totalSessions = state.sessions;
   return {
     sessions: totalSessions,
     total: selectUserTotal(totalSessions),
     types: selectTypes(totalSessions),
     progress: selectProgress(totalSessions)
-  }
+  };
 };
 
 export default connect(mapStateToProps)(SessionsGraphPage);
