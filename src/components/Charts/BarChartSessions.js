490b2c7d7bd946c10168b4bfe9b9c065bdaa7898
Antonija Simic <antonija1023@gmail.com>
2019-05-04 16:13:10 +0200

    wrap graphs into responsive container

--- src/components/Charts/BarChartSessions.js
+++ src/components/Charts/BarChartSessions.js
@@ -1,28 +1,39 @@
 import React from "react";
-import { BarChart, CartesianGrid, XAxis, YAxis, Tooltip, Bar } from "recharts";
+import {
+  BarChart,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Bar,
+  ResponsiveContainer
+} from "recharts";
 import moment from "moment";
 
 const BarChartSessions = ({ data }) => (
-  <BarChart width={730} height={250} data={data}>
-    <CartesianGrid strokeDasharray="3 3" />
-    <XAxis dataKey="email" hide={true} />
-    <YAxis
-      label={{ value: "hours", angle: -90, position: "insideLeft" }}
-      tickFormatter={value => {
-        let formated = moment.duration(value, "seconds");
-        return formated._data.hours;
-      }}
-    />
-    <Tooltip
-      formatter={value => {
-        let formated = moment.duration(value, "seconds");
-        return `${formated._data.hours}h:${formated._data.minutes}m:${
-          formated._data.seconds
-        }s`;
-      }}
-    />
-    <Bar dataKey="duration" fill="#2ec4b6" />
-  </BarChart>
+  <ResponsiveContainer width="100%" height={200}>
+    <BarChart data={data} style={{ marginTop: 100 }}>
+      {console.log(data)}
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="email" hide={true} />
+      <YAxis
+        label={{ value: "hours", angle: -90, position: "insideLeft" }}
+        tickFormatter={value => {
+          let formated = moment.duration(value, "seconds");
+          return formated._data.hours;
+        }}
+      />
+      <Tooltip
+        formatter={value => {
+          let formated = moment.duration(value, "seconds");
+          return `${formated._data.hours}h:${formated._data.minutes}m:${
+            formated._data.seconds
+          }s`;
+        }}
+      />
+      <Bar dataKey="duration" fill="#2ec4b6" />
+    </BarChart>
+  </ResponsiveContainer>
 );
 
 export default BarChartSessions;
