b401b4a177a2b9beb0e9a486c41d479ff69db107
Antonija Simic <antonija1023@gmail.com>
2019-05-11 14:26:16 +0200

    show username for guest

--- src/components/LoggedInAs.js
+++ src/components/LoggedInAs.js
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 const LoggedInAs = ({ email }) => <p> Logged in as {email}</p>;
 
 const mapStateToProps = (state, props) => ({
-  email: state.auth.email
+  email: state.auth.email || state.auth.id
 });
 
 export default connect(mapStateToProps)(LoggedInAs);
