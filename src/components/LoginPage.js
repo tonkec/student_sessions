c8c1e5cee1e25e79a17fd77a2d0b340b29ef9b4a
Antonija Simic <antonija1023@gmail.com>
2019-05-13 20:57:29 +0200

    remove brackets from prettier

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -7,8 +7,7 @@ const LoginPage = () => {
     <div className="login-page">
       <h1 className="m-b-0"> Sessions Tracker </h1>
       <p>
-        {" "}
-        Track mentoring sessions with your students and get the meaningful data{" "}
+        Track mentoring sessions with your students and get the meaningful data
       </p>
       <LoginWithGoogle />
       <LoginAsGuest />
